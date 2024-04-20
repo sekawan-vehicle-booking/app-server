e6d2fd1b5d96fc3a3bf5344abb06f8a9fbf645c2
TamaWira <asusrog20012001@gmail.com>
2024-04-20 14:46:46 +0800

    feat: add vehicle services migration

--- src/migrations/003_vehicleServices.ts
+++ src/migrations/003_vehicleServices.ts
@@ -1,10 +1,19 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(
+    "vehicle_services",
+    (table: Knex.TableBuilder) => {
+      table.string("id", 16).primary();
+      table.string("idCar", 16).notNullable();
+      table.timestamp("serviceDate").notNullable();
+      table.string("serviceType", 16).notNullable();
+      table.integer("serviceCost").notNullable();
+      table.timestamps(true, true);
+    }
+  );
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("vehicle_services");
 }
-
