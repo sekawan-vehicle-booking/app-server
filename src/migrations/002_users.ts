ced321af6bd5657d6aaba87c03ecd4d168147d84
TamaWira <asusrog20012001@gmail.com>
2024-04-20 14:22:22 +0800

    feat: update users migration

--- src/migrations/002_users.ts
+++ src/migrations/002_users.ts
@@ -1,10 +1,14 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("users", (table: Knex.TableBuilder) => {
+    table.string("id", 16).primary();
+    table.string("name", 255).notNullable();
+    table.enum("role", ["Employee", "Admin", "Supervisor"]).notNullable();
+    table.timestamps(true, true);
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("users");
 }
-
