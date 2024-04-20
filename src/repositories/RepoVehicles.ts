75054ff221f224d5e4195d7467dbbd50c2d37cdc
TamaWira <asusrog20012001@gmail.com>
2024-04-20 15:20:11 +0800

    feat: add repo vehicle

---
+++ src/repositories/RepoVehicles.ts
@@ -0,0 +1,29 @@
+import Vehicles, { IVehicle } from "../models/Vehicles";
+
+class RepoVehicles {
+  constructor() {}
+
+  // #region List
+  async list() {
+    const vehicles = Vehicles.query();
+    return vehicles;
+  }
+
+  //   #region Find
+  async findById(id: string) {
+    const vehicle = Vehicles.query().findById(id);
+    return vehicle;
+  }
+
+  //   #region Update
+  async update(id: string, params: IVehicle) {
+    const vehicle = Vehicles.query().findById(id).patch(params);
+    return vehicle;
+  }
+
+  //   #region Delete
+  async delete(id: string) {
+    const vehicle = Vehicles.query().deleteById(id);
+    return vehicle;
+  }
+}
