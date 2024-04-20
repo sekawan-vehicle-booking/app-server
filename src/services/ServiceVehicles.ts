f5b3129811e46c98c70b962fdb965888bb1db8d7
TamaWira <asusrog20012001@gmail.com>
2024-04-20 15:26:39 +0800

    feat: add service vehicles

---
+++ src/services/ServiceVehicles.ts
@@ -0,0 +1,36 @@
+import { IVehicle } from "../models/Vehicles";
+import RepoVehicles from "../repositories/RepoVehicles";
+
+class ServiceVehicles {
+  private _repoVehicles: RepoVehicles;
+
+  constructor(repoVehicles: RepoVehicles) {
+    this._repoVehicles = repoVehicles;
+  }
+
+  //   #region List
+  async list() {
+    const vehicles = this._repoVehicles.list();
+    return vehicles;
+  }
+
+  //   #region Find
+  async find(id: string) {
+    const vehicle = this._repoVehicles.findById(id);
+    return vehicle;
+  }
+
+  //   #region Update
+  async update(id: string, params: IVehicle) {
+    const vehicle = this._repoVehicles.update(id, params);
+    return vehicle;
+  }
+
+  //   #region Delete
+  async delete(id: string) {
+    const vehicle = this._repoVehicles.delete(id);
+    return vehicle;
+  }
+}
+
+export default ServiceVehicles;
